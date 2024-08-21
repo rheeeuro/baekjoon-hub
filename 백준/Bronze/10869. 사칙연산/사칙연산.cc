8fb3d1852570e4e390bc622f3eebc0725257fd5d
Rhee Euro <rheeeuro@gmail.com>
2024-08-22 01:01:12 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void) {
+	int a = 0, b = 0;
+	scanf("%d", &a);
+	scanf("%d", &b);
+	printf("%d \n", a + b);
+	printf("%d \n", a - b);
+	printf("%d \n", a * b);
+	printf("%d \n", a / b);
+	printf("%d \n", a % b);
+	return 0;
+}
